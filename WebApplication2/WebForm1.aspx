b5db947bcc4ff51308bc64e7e8598e730c9c5443
umeshvicky17 <umeshschaudhari1@gmail.com>
2023-12-27 17:36:58 +0530

    Add project files.

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,127 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 459px;
+        }
+        .auto-style3 {
+            width: 570px;
+        }
+        .auto-style4 {
+            width: 459px;
+            height: 26px;
+        }
+        .auto-style5 {
+            width: 570px;
+            height: 26px;
+        }
+        .auto-style6 {
+            height: 26px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label1" runat="server" Text="EmpId"></asp:Label>
+                </td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+                <td rowspan="6">
+                    <asp:GridView ID="GridView1" runat="server" Height="334px" Width="653px">
+                    </asp:GridView>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label2" runat="server" Text="EmpName"></asp:Label>
+                </td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label3" runat="server" Text="Designation"></asp:Label>
+                </td>
+                <td class="auto-style3">
+                    <asp:DropDownList ID="DropDownList1" runat="server">
+                        <asp:ListItem>developer</asp:ListItem>
+                        <asp:ListItem>engineer</asp:ListItem>
+                        <asp:ListItem>doctor</asp:ListItem>
+                        <asp:ListItem></asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label4" runat="server" Text="skill"></asp:Label>
+                </td>
+                <td class="auto-style3">
+                    <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+                        <asp:ListItem>java</asp:ListItem>
+                        <asp:ListItem>dot net</asp:ListItem>
+                        <asp:ListItem>python</asp:ListItem>
+                        <asp:ListItem>php</asp:ListItem>
+                    </asp:CheckBoxList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label5" runat="server" Text="DOB"></asp:Label>
+                </td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="SearchbyEmp" />
+                </td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style3">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style3">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="Update" Width="262px" />
+                </td>
+                <td class="auto-style3">
+                    <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Delete" />
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="save" />
+                    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="showall" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style4"></td>
+                <td class="auto-style5">
+                    <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label>
+                </td>
+                <td class="auto-style6"></td>
+            </tr>
+        </table>
+    </form>
+    </body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static String cnstr = "Server=UMESH-PC\\sqlexpress;database=crudedb;Integrated security = true";
+        SqlConnection mycnn = new SqlConnection(cnstr);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand mycmd = new SqlCommand();
+                mycnn.Open();
+                mycmd.Connection = mycnn;
+                mycmd.CommandType = CommandType.StoredProcedure;
+                mycmd.Parameters.AddWithValue("@p1", TextBox1.Text);
+                mycmd.Parameters.AddWithValue("@p2", TextBox2.Text);
+                mycmd.Parameters.AddWithValue("@p3", DropDownList1.Text);
+                String qual = null;
+                for (int i = 0; i < CheckBoxList1.Items.Count; i++)
+                {
+                    if (CheckBoxList1.Items[i].Selected)
+                        qual += CheckBoxList1.Items[i].Text;
+                }
+                mycmd.Parameters.AddWithValue("@p4", qual);
+                mycmd.Parameters.AddWithValue("@p5", TextBox5.Text);
+                mycmd.CommandText = "insertEmpRecord";
+                mycmd.ExecuteNonQuery();
+                Label6.Text = ("empyee record added");
+                mycnn.Close();
+
+            }
+            catch (SqlException ss)
+            {
+                Label6.Text = (" wrong ");
+
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand mycmd = new SqlCommand();
+                mycnn.Open();
+                mycmd.Connection = mycnn;
+                mycmd.CommandType = CommandType.StoredProcedure;
+                mycmd.Parameters.AddWithValue("@p1", TextBox4.Text);
+
+
+                mycmd.CommandText = "SearchEmp";
+                SqlDataReader rd = mycmd.ExecuteReader();
+
+
+                if (rd.HasRows)
+                {
+                    rd.Read();
+                   TextBox1.Text = rd.GetInt32(0).ToString();
+                    TextBox2.Text = rd.GetString(1);
+                    DropDownList1.Text = rd.GetString(2);
+                 
+                    TextBox5.Text = rd.GetDateTime(4).ToString();
+                   
+
+                }
+
+            }
+            catch (SqlException ss)
+             
+            {
+                Label6.Text = (" wrong ");
+
+            }
+            mycnn.Close();
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand mycmd = new SqlCommand();
+                mycnn.Open();
+                mycmd.Connection = mycnn;
+                mycmd.CommandType = CommandType.StoredProcedure;
+                mycmd.Parameters.AddWithValue("@p1", TextBox1.Text);
+                mycmd.Parameters.AddWithValue("@p2", DropDownList1.Text);
+                String qual=null;
+                for(int i=0;i<CheckBoxList1.Items.Count;i++)
+                {
+                    if(CheckBoxList1.Items[i].Selected)
+                    qual += CheckBoxList1.Items[i].Text;
+                }
+                mycmd.Parameters.AddWithValue("@p3", qual);
+                mycmd.CommandText = "updateEmpRecord";
+                mycmd.ExecuteNonQuery();
+                Label6.Text = ("empyee record updated");
+
+            }
+            catch (SqlException ss)
+            {
+                Label6.Text = (" wrong ");
+            }
+            mycnn.Close();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand mycmd = new SqlCommand();
+                mycnn.Open();
+
+                mycmd.Connection = mycnn;
+                mycmd.CommandType = CommandType.StoredProcedure;
+                mycmd.Parameters.AddWithValue("@p1", TextBox4.Text);
+
+                mycmd.CommandText = "DeleteEmpRecord";
+                mycmd.ExecuteNonQuery();
+                Label6.Text = ("empyee record deleted");
+
+            }
+            catch (SqlException ss)
+            {
+
+                Label6.Text = (" wrong ");
+            }
+            mycnn.Close();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand mycmd = new SqlCommand();
+                mycnn.Open();
+                mycmd.Connection = mycnn;
+                mycmd.CommandType = CommandType.StoredProcedure;
+
+
+
+                mycmd.CommandText = "ShowEmpRecords";
+                SqlDataAdapter adp = new SqlDataAdapter();
+                adp.SelectCommand = mycmd;
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "Emp");
+              DropDownList1.DataSource= ds.Tables["Emp"];
+               GridView1.DataSource = ds.Tables["Emp"];
+                GridView1.DataBind();
+            
+
+            }
+            catch (SqlException ss)
+            {
+
+                Label6.Text = (" wrong ");
+            }
+            mycnn.Close();
+        }
+    }
+}
